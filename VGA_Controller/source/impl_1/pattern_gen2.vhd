2c49d94e784285d0f7ec2d2832e2a6216987c3dc
Vivian Lau <vivian@Vivians-MacBook.local>
2024-12-10 02:46:22 -0500

    fixed arrows

--- VGA_Controller/source/impl_1/pattern_gen2.vhd
+++ VGA_Controller/source/impl_1/pattern_gen2.vhd
@@ -25,24 +25,24 @@ end pattern_gen;
 architecture synth of pattern_gen is
 signal rom_data: std_logic_vector(5 downto 0);
 signal end_screen: std_logic_vector(5 downto 0);
-signal rom_arrow_1: std_logic_vector(5 downto 0);
-signal rom_arrow_2: std_logic_vector(5 downto 0);
-signal rom_arrow_3: std_logic_vector(5 downto 0);
-signal rom_arrow_4: std_logic_vector(5 downto 0);
+signal leftarr_data: std_logic_vector(5 downto 0);
+signal downarr_data: std_logic_vector(5 downto 0);
+signal rightarr_data: std_logic_vector(5 downto 0);
+signal uparr_data: std_logic_vector(5 downto 0);
 signal rom_top_out: std_logic_vector(5 downto 0);
 signal rom_left_out: std_logic_vector(5 downto 0);
 signal rom_right_out: std_logic_vector(5 downto 0);
 signal rom_down_out: std_logic_vector(5 downto 0);
 signal score_data: std_logic_vector(5 downto 0);
 
-signal drawa1x: std_logic_vector(9 downto 0);
-signal drawa2x: std_logic_vector(9 downto 0);
-signal drawa3x: std_logic_vector(9 downto 0);
-signal drawa4x: std_logic_vector(9 downto 0);
-signal drawa1y: std_logic_vector(9 downto 0);
-signal drawa2y: std_logic_vector(9 downto 0);
-signal drawa3y: std_logic_vector(9 downto 0);
-signal drawa4y: std_logic_vector(9 downto 0);
+signal draw_leftx: std_logic_vector(9 downto 0);
+signal draw_upx: std_logic_vector(9 downto 0);
+signal draw_rightx: std_logic_vector(9 downto 0);
+signal draw_downx: std_logic_vector(9 downto 0);
+signal draw_upy: std_logic_vector(9 downto 0);
+signal draw_lefty: std_logic_vector(9 downto 0);
+signal draw_righty: std_logic_vector(9 downto 0);
+signal draw_downy: std_logic_vector(9 downto 0);
 
 signal drawtopx: std_logic_vector(9 downto 0);
 signal drawleftx: std_logic_vector(9 downto 0);
@@ -54,57 +54,31 @@ signal drawrighty: std_logic_vector(9 downto 0);
 signal drawdowny: std_logic_vector(9 downto 0);
 
 constant left_arr_xpos      : integer := 100;
-constant right_arr_xpos        : integer := 220; 
-constant top_arr_xpos   : integer := 340; 
+constant top_arr_xpos        : integer := 220; 
+constant right_arr_xpos   : integer := 340; 
 constant down_arr_xpos   : integer := 460;
 
-constant top_out_xpos : integer := 340;
-constant left_out_xpos : integer := 100;
-constant right_out_xpos : integer := 220;
-constant down_out_xpos : integer := 460;
-
-constant top_out_ypos : integer := 420;
-constant left_out_ypos : integer := 420;
-constant right_out_ypos : integer := 420;
-constant down_out_ypos : integer := 420;
+constant out_ypos : integer := 420;
 
 constant score_y1_pos: integer := 4;
 constant score_y2_pos: integer := 480;
 
 constant H_SYNC_START   : integer := 656; 
 
-component arrow1 is 
+component arrow_rom is 
    port(
 		outglobal_o : in std_logic;
-		addr_x : in std_logic_vector(7 downto 0);
-		addr_y : in std_logic_vector(7 downto 0);
+		addr_x : in std_logic_vector(4 downto 0);
+		addr_y : in std_logic_vector(4 downto 0);
 		data : out std_logic_vector(5 downto 0)
 		);
 	end component;
 	
-	component arrow2 is 
+component outline_rom is 
    port(
 		outglobal_o : in std_logic;
-		addr_x : in std_logic_vector(7 downto 0);
-		addr_y : in std_logic_vector(7 downto 0);
-		data : out std_logic_vector(5 downto 0)
-		);
-	end component;
-	
-	component arrow3 is 
-   port(
-		outglobal_o : in std_logic;
-		addr_x : in std_logic_vector(7 downto 0);
-		addr_y : in std_logic_vector(7 downto 0);
-		data : out std_logic_vector(5 downto 0)
-		);
-	end component;
-	
-	component arrow4 is 
-   port(
-		outglobal_o : in std_logic;
-		addr_x : in std_logic_vector(7 downto 0);
-		addr_y : in std_logic_vector(7 downto 0);
+		addr_x : in std_logic_vector(4 downto 0);
+		addr_y : in std_logic_vector(4 downto 0);
 		data : out std_logic_vector(5 downto 0)
 		);
 	end component;
@@ -127,41 +101,6 @@ component endscreen is
        	);
        end component;
 
-	component top_outline is 
-   port(
-		outglobal_o : in std_logic;
-		addr_x : in std_logic_vector(7 downto 0);
-		addr_y : in std_logic_vector(7 downto 0);
-		data : out std_logic_vector(5 downto 0)
-		);
-	end component;
-	
-	component left_outline is 
-   port(
-		outglobal_o : in std_logic;
-		addr_x : in std_logic_vector(7 downto 0);
-		addr_y : in std_logic_vector(7 downto 0);
-		data : out std_logic_vector(5 downto 0)
-		);
-	end component;
-	
-	component right_outline is 
-   port(
-		outglobal_o : in std_logic;
-		addr_x : in std_logic_vector(7 downto 0);
-		addr_y : in std_logic_vector(7 downto 0);
-		data : out std_logic_vector(5 downto 0)
-		);
-	end component;
-	
-	component down_outline is 
-   port(
-		outglobal_o : in std_logic;
-		addr_x : in std_logic_vector(7 downto 0);
-		addr_y : in std_logic_vector(7 downto 0);
-		data : out std_logic_vector(5 downto 0)
-		);
-	end component;
 	
 	component score_rom is 
     port(
@@ -186,52 +125,52 @@ begin
 		addr_y => pixel_y(9 downto 3), 
 		data => end_screen);
 
-		myarrow1: arrow1 port map(
+		leftarr_inst: arrow_rom port map(
 		outglobal_o => outglobal_o, 
-		addr_x => drawa1x(9 downto 2), 
-		addr_y => drawa1y(9 downto 2), 
-		data => rom_arrow_1);
+		addr_x => draw_leftx(6 downto 2), 
+		addr_y => draw_lefty(6 downto 2), 
+		data => leftarr_data);
 
-		myarrow2: arrow2 port map(
+		uparr_inst: arrow_rom port map(
 		outglobal_o => outglobal_o, 
-		addr_x => drawa2x(9 downto 2), 
-		addr_y => drawa2y(9 downto 2), 
-		data => rom_arrow_2);
+		addr_x => draw_upx(6 downto 2), 
+		addr_y => draw_upy(6 downto 2), 
+		data => uparr_data);
 
-		myarrow3: arrow3 port map(
+		rightarr_inst: arrow_rom port map(
 		outglobal_o => outglobal_o, 
-		addr_x => drawa3x(9 downto 2), 
-		addr_y => drawa3y(9 downto 2), 
-		data => rom_arrow_3);
+		addr_x => draw_rightx(6 downto 2), 
+		addr_y => draw_righty(6 downto 2), 
+		data => rightarr_data);
 
-		myarrow4: arrow4 port map(
+		downarr_inst: arrow_rom port map(
 		outglobal_o => outglobal_o, 
-		addr_x => drawa4x(9 downto 2), 
-		addr_y => drawa4y(9 downto 2), 
-		data => rom_arrow_4);
+		addr_x => draw_downx(6 downto 2), 
+		addr_y => draw_downy(6 downto 2), 
+		data => downarr_data);
 		
-		mytopout: top_outline port map(
+		mytopout: outline_rom port map(
 		outglobal_o => outglobal_o, 
-		addr_x => drawtopx(9 downto 2), 
-		addr_y => drawtopy(9 downto 2), 
+		addr_x => drawtopx(6 downto 2),
+		addr_y => drawtopy(6 downto 2),
 		data => rom_top_out);
 
-		mydownout: down_outline port map(
+		mydownout: outline_rom port map(
 		outglobal_o => outglobal_o, 
-		addr_x => drawdownx(9 downto 2), 
-		addr_y => drawdowny(9 downto 2), 
+		addr_x => drawdownx(6 downto 2), 
+		addr_y => drawdowny(6 downto 2), 
 		data => rom_down_out);
 
-		myrightout: right_outline port map(
+		myrightout: outline_rom port map(
 		outglobal_o => outglobal_o, 
-		addr_x => drawrightx(9 downto 2), 
-		addr_y => drawrighty(9 downto 2), 
+		addr_x => drawrightx(6 downto 2), 
+		addr_y => drawrighty(6 downto 2), 
 		data => rom_right_out);
 
-		myleftout: left_outline port map(
+		myleftout: outline_rom port map(
 		outglobal_o => outglobal_o, 
-		addr_x => drawleftx(9 downto 2), 
-		addr_y => drawlefty(9 downto 2), 
+		addr_x => drawleftx(6 downto 2), 
+		addr_y => drawlefty(6 downto 2), 
 		data => rom_left_out);
 		
 		myscore: score_rom port map(
@@ -242,23 +181,23 @@ begin
 		addr_y => pixel_y,
     	data => score_data);
 	
-	drawa1x <= std_logic_vector(unsigned(pixel_x) - left_arr_xpos);
-	drawa1y <= std_logic_vector(unsigned(pixel_y) - unsigned(left_arr_ypos));
-	drawa2x <= std_logic_vector(unsigned(pixel_x) - top_arr_xpos);
-	drawa2y <= std_logic_vector(unsigned(pixel_y) - unsigned(top_arr_ypos));
-	drawa3x <= std_logic_vector(unsigned(pixel_x) - right_arr_xpos);
-	drawa3y <= std_logic_vector(unsigned(pixel_y) - unsigned(right_arr_ypos));
-	drawa4x <= std_logic_vector(unsigned(pixel_x) - down_arr_xpos);
-	drawa4y <= std_logic_vector(unsigned(pixel_y) - unsigned(down_arr_ypos));
+	draw_leftx <= std_logic_vector(unsigned(pixel_x) - left_arr_xpos);
+	draw_lefty <= std_logic_vector(unsigned(pixel_y) - unsigned(left_arr_ypos));
+	draw_upx <= std_logic_vector(unsigned(pixel_y) - unsigned(top_arr_ypos));
+	draw_upy <= std_logic_vector(unsigned(pixel_x) - top_arr_xpos);
+	draw_rightx <= std_logic_vector(60 - (unsigned(pixel_x) - right_arr_xpos));
+	draw_righty <= std_logic_vector(unsigned(pixel_y) - unsigned(right_arr_ypos));
+	draw_downx <= std_logic_vector(60 - (unsigned(pixel_y) - unsigned(down_arr_ypos)));
+	draw_downy <= std_logic_vector(unsigned(pixel_x) - down_arr_xpos);
 	
-	drawtopx <= std_logic_vector(unsigned(pixel_x) - top_out_xpos);
-	drawtopy <= std_logic_vector(unsigned(pixel_y) - top_out_ypos);
-	drawleftx <= std_logic_vector(unsigned(pixel_x) - left_out_xpos);
-	drawlefty <= std_logic_vector(unsigned(pixel_y) - left_out_ypos);
-	drawrightx <= std_logic_vector(unsigned(pixel_x) - right_out_xpos);
-	drawrighty <= std_logic_vector(unsigned(pixel_y) - right_out_ypos);
-	drawdownx <= std_logic_vector(unsigned(pixel_x) - down_out_xpos);
-	drawdowny <= std_logic_vector(unsigned(pixel_y) - down_out_ypos);
+	drawtopx <= std_logic_vector(unsigned(pixel_y) - out_ypos);
+	drawtopy <= std_logic_vector(unsigned(pixel_x) - top_arr_xpos);
+	drawleftx <= std_logic_vector(unsigned(pixel_x) - left_arr_xpos);
+	drawlefty <= std_logic_vector(unsigned(pixel_y) - out_ypos);
+	drawrightx <= std_logic_vector(60 - (unsigned(pixel_x) - right_arr_xpos));
+	drawrighty <= std_logic_vector(unsigned(pixel_y) - out_ypos);
+	drawdownx <= std_logic_vector(60 - (unsigned(pixel_y) - out_ypos));
+	drawdowny <= std_logic_vector(unsigned(pixel_x) - down_arr_xpos);
 	
 	process(valid, pixel_x, pixel_y, outglobal_o) begin
 	if rising_edge(outglobal_o) then
@@ -266,22 +205,42 @@ begin
 			if flag = "00" then
 				rgb <= rom_data;
 			elsif flag = "01" then
-				if to_integer(unsigned(pixel_x)) > left_arr_xpos - 1 and to_integer(unsigned(pixel_x)) < left_arr_xpos + 60 and to_integer(unsigned(pixel_y)) > to_integer(unsigned(left_arr_ypos)) - 1 and to_integer(unsigned(pixel_y)) < to_integer(unsigned(left_arr_ypos)) + 60 and arrows_spawned(0) = '1' then
-					rgb <= rom_arrow_1;  
-				elsif to_integer(unsigned(pixel_x)) > top_arr_xpos - 1 and to_integer(unsigned(pixel_x)) < top_arr_xpos + 60 and to_integer(unsigned(pixel_y)) > to_integer(unsigned(top_arr_ypos)) - 1 and to_integer(unsigned(pixel_y)) < to_integer(unsigned(top_arr_ypos)) + 60 and arrows_spawned(1) = '1' then
-					rgb <= rom_arrow_2;
-				elsif to_integer(unsigned(pixel_x)) > right_arr_xpos - 1 and to_integer(unsigned(pixel_x)) < right_arr_xpos + 60 and to_integer(unsigned(pixel_y)) > to_integer(unsigned(right_arr_ypos)) - 1 and to_integer(unsigned(pixel_y)) < to_integer(unsigned(right_arr_ypos)) + 60 and arrows_spawned(2) = '1' then
-					rgb <= rom_arrow_3;
-				elsif to_integer(unsigned(pixel_x)) > down_arr_xpos - 1 and to_integer(unsigned(pixel_x)) < down_arr_xpos + 60 and to_integer(unsigned(pixel_y)) > to_integer(unsigned(down_arr_ypos)) - 1 and to_integer(unsigned(pixel_y)) < to_integer(unsigned(down_arr_ypos)) + 60 and arrows_spawned(3) = '1'  then
-					rgb <= rom_arrow_4;
-				elsif to_integer(unsigned(pixel_x)) > top_out_xpos - 1 and to_integer(unsigned(pixel_x)) < top_out_xpos + 60 and to_integer(unsigned(pixel_y)) > top_out_ypos - 1 and to_integer(unsigned(pixel_y)) < top_out_ypos + 60 then
+				if to_integer(unsigned(pixel_x)) > top_arr_xpos - 1 and to_integer(unsigned(pixel_x)) < top_arr_xpos + 60 
+								and to_integer(unsigned(pixel_y)) > out_ypos - 1 and to_integer(unsigned(pixel_y)) < out_ypos + 60 
+								and not(rom_top_out = "000001") then
 					rgb <= rom_top_out;  
-				elsif to_integer(unsigned(pixel_x)) > left_out_xpos - 1 and to_integer(unsigned(pixel_x)) < left_out_xpos + 60 and to_integer(unsigned(pixel_y)) > left_out_ypos - 1 and to_integer(unsigned(pixel_y)) < left_out_ypos + 60 then
+				elsif to_integer(unsigned(pixel_x)) > left_arr_xpos - 1 and to_integer(unsigned(pixel_x)) < left_arr_xpos + 60 
+								and to_integer(unsigned(pixel_y)) > out_ypos - 1 and to_integer(unsigned(pixel_y)) < out_ypos + 60
+								and not(rom_left_out = "000001") then
 					rgb <= rom_left_out;
-				elsif to_integer(unsigned(pixel_x)) > down_out_xpos - 1 and to_integer(unsigned(pixel_x)) < down_out_xpos + 60 and to_integer(unsigned(pixel_y)) > down_out_ypos - 1 and to_integer(unsigned(pixel_y)) < down_out_ypos + 60 then
+				elsif to_integer(unsigned(pixel_x)) > down_arr_xpos - 1 and to_integer(unsigned(pixel_x)) < down_arr_xpos + 60 
+								and to_integer(unsigned(pixel_y)) > out_ypos - 1 and to_integer(unsigned(pixel_y)) < out_ypos + 60 
+								and not(rom_down_out = "000001") then
 					rgb <= rom_down_out;
-				elsif to_integer(unsigned(pixel_x)) > right_out_xpos - 1 and to_integer(unsigned(pixel_x)) < right_out_xpos + 60 and to_integer(unsigned(pixel_y)) > right_out_ypos - 1 and to_integer(unsigned(pixel_y)) < right_out_ypos + 60 then
+				elsif to_integer(unsigned(pixel_x)) > right_arr_xpos - 1 and to_integer(unsigned(pixel_x)) < right_arr_xpos + 60 
+								and to_integer(unsigned(pixel_y)) > out_ypos - 1 and to_integer(unsigned(pixel_y)) < out_ypos + 60
+								and not(rom_right_out = "000001") then
 					rgb <= rom_right_out;
+				elsif to_integer(unsigned(pixel_x)) > left_arr_xpos - 1 and to_integer(unsigned(pixel_x)) < left_arr_xpos + 60 
+								and to_integer(unsigned(pixel_y)) > to_integer(unsigned(left_arr_ypos)) - 1 
+								and to_integer(unsigned(pixel_y)) < to_integer(unsigned(left_arr_ypos)) + 60 
+								and arrows_spawned(0) = '1' and not(leftarr_data = "000001") then
+					rgb <= leftarr_data;  
+				elsif to_integer(unsigned(pixel_x)) > top_arr_xpos - 1 and to_integer(unsigned(pixel_x)) < top_arr_xpos + 60 
+								and to_integer(unsigned(pixel_y)) > to_integer(unsigned(top_arr_ypos)) - 1 
+								and to_integer(unsigned(pixel_y)) < to_integer(unsigned(top_arr_ypos)) + 60 
+								and arrows_spawned(1) = '1' and not(uparr_data = "000001") then
+					rgb <= uparr_data;
+				elsif to_integer(unsigned(pixel_x)) > right_arr_xpos - 1 and to_integer(unsigned(pixel_x)) < right_arr_xpos + 60 
+								and to_integer(unsigned(pixel_y)) > to_integer(unsigned(right_arr_ypos)) - 1 
+								and to_integer(unsigned(pixel_y)) < to_integer(unsigned(right_arr_ypos)) + 60 
+								and arrows_spawned(2) = '1' and not(rightarr_data = "000001") then
+					rgb <= rightarr_data;
+				elsif to_integer(unsigned(pixel_x)) > down_arr_xpos - 1 and to_integer(unsigned(pixel_x)) < down_arr_xpos + 60 
+								and to_integer(unsigned(pixel_y)) > to_integer(unsigned(down_arr_ypos)) - 1 
+								and to_integer(unsigned(pixel_y)) < to_integer(unsigned(down_arr_ypos)) + 60 
+								and arrows_spawned(3) = '1' and not(downarr_data = "000001") then
+					rgb <= downarr_data;
 				elsif to_integer(unsigned(pixel_y)) > score_y1_pos - 1 and to_integer(unsigned(pixel_y)) < score_y2_pos + 1 then
 					rgb <= score_data;
 				else
